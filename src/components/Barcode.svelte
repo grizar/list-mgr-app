42a39009727eb1a52f6d5583a8eb6e86d9590a50
Alain Roghi <alain.roghi@gmail.com>
2021-04-29 21:59:42 +0200

    DeviceId

--- src/components/Barcode.svelte
+++ src/components/Barcode.svelte
@@ -1,90 +1,95 @@
 <script>
-  import { onMount } from 'svelte';
-  import { onDestroy } from 'svelte';
-  import { createEventDispatcher } from 'svelte';
+  import { onMount } from "svelte";
+  import { onDestroy } from "svelte";
+  import { createEventDispatcher } from "svelte";
 
-	const dispatch = createEventDispatcher();
-  
-  import Quagga from '@ericblade/quagga2';
+  const dispatch = createEventDispatcher();
+
+  import Quagga from "@ericblade/quagga2";
 
   let wrapper;
-
-  var backCamID = null;
-  var last_camera = null;
-  navigator.mediaDevices.enumerateDevices()
-  .then(function(devices) {
-    devices.forEach(function(device) {  
-      console.log(device);
-      if( device.kind == "videoinput" && device.label.match(/back/) !== null ){
-            backCamID = device.deviceId;
-      }
-      if( device.kind === "videoinput"){
-        last_camera = device.deviceId;
-      } 
-    });
-  
-    if( backCamID === null){
-      backCamID = last_camera;
-    }
-  })
-  .catch(function(err) {         });
-  console.log('Camera selected');
-  console.log(backCamID);
 
   function barcode(code) {
     console.log(code);
     Quagga.stop();
-    console.log('dispatching message');
-		dispatch('message', {
-      type: 'code',
-			code: code.codeResult.code
-		});
-	}
+    console.log("dispatching message");
+    dispatch("message", {
+      type: "code",
+      code: code.codeResult.code,
+    });
+  }
 
   onDestroy(() => {
     Quagga.stop();
   });
 
   onMount(() => {
-    console.log(wrapper);
-    console.log('Camera');
-    console.log(backCamID);
-    Quagga.init({
-      inputStream : {
-        name : "Live",
-        type : "LiveStream",
-        target: wrapper
-      },
-      constraints: {
-        width: 320,
-        height: 240,
-        video: {
-          mandatory: {
-            minWidth: { min: 1280 },
-            minHeight: { min: 720 }
+    var backCamID = null;
+    var last_camera = null;
+    navigator.mediaDevices
+      .enumerateDevices()
+      .then(function (devices) {
+        devices.forEach(function (device) {
+          console.log(device);
+          if (
+            device.kind == "videoinput" &&
+            device.label.match(/back/) !== null
+          ) {
+            backCamID = device.deviceId;
+          }
+          if (device.kind === "videoinput") {
+            last_camera = device.deviceId;
+          }
+        });
+
+        if (backCamID === null) {
+          backCamID = last_camera;
+        }
+
+        console.log("Camera selected");
+        console.log(backCamID);
+
+        console.log(wrapper);
+        console.log("Camera");
+        console.log(backCamID);
+        Quagga.init(
+          {
+            inputStream: {
+              name: "Live",
+              type: "LiveStream",
+              target: wrapper,
+            },
+            constraints: {
+              width: 320,
+              height: 240,
+              video: {
+                mandatory: {
+                  minWidth: { min: 1280 },
+                  minHeight: { min: 720 },
+                },
+                facingMode: "environment",
+                deviceId: backCamID,
+              },
+            },
+            showCanvas: false,
+            locator: false,
+            decoder: {
+              readers: ["ean_reader"],
+            },
           },
-        facingMode: "environment",
-        deviceId: backCamID
-        }
-
-      },
-      showCanvas: false,
-      locator: false,
-      decoder : {
-        readers : ["ean_reader"]
-      }
-    }, function(err) {
-        if (err) {
-            console.log(err);
-            return
-        }
-        console.log("Initialization finished. Ready to start");
-        Quagga.start();
-        Quagga.onDetected(barcode);
-
-    });
-  })
-
+          function (err) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("Initialization finished. Ready to start");
+            Quagga.start();
+            Quagga.onDetected(barcode);
+          }
+        );
+      })
+      .catch(function (err) {});
+  });
 </script>
 
-<div bind:this="{wrapper}"></div>
+<div bind:this={wrapper} />
